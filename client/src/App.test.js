65915fd4707bc77fcc535153d4ab6f811a61f651
Kim Greenbush <kim_greenbush@icloud.com>
2021-07-09 16:54:08 -0700

    add smoke test for app.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,8 +5,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import renderer from "react-test-renderer";
 
 it("renders without crashing", () => {
 	const div = document.createElement("div");
 	ReactDOM.render(<App />, div);
 });
+
+test("App get pirates", () => {
+	const component = renderer.create(
+		<App />,
+	)
+	let tree = component.toJSON()
+	expect(tree).toMatchSnapshot();
+})
