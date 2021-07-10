e85e47589b711087802a7926f749a4c854e49f62
Kim Greenbush <kim_greenbush@icloud.com>
2021-07-09 19:01:50 -0700

    initialize test for AddForm

---
+++ client/src/views/AddForm.test.js
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import AddForm from "./AddForm.jsx";
+import renderer from "react-test-renderer";
+
+it("renders without crashing", () => {
+	const div = document.createElement("div");
+	ReactDOM.render(<AddForm />, div);
+});
+
+// test("AddForm get pirates", () => {
+// 	const component = renderer.create(<AddForm />);
+// 	let tree = component.toJSON();
+//     expect(tree).toMatchSnapshot();
+
+//     tree.props.clearForm()
+//     tree.toJSON()
+//     expect(tree).toMatchSnapshot()
+// });
