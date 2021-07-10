59a119ba439b01c4d0609e7c9c025fcf3acbc1f1
Kim Greenbush <kim_greenbush@icloud.com>
2021-07-09 19:01:37 -0700

    update api routes

--- server/routes/pirate.routes.js
+++ server/routes/pirate.routes.js
@@ -1,9 +1,9 @@
 const PirateController = require("../controllers/pirate.controller");
 
 module.exports = function (app) {
-	app.get("/api/pirates", PirateController.index);
-	app.post("/api/pirates/new", PirateController.createPirate);
-	app.get("/api/pirates/:id", PirateController.singlePirate);
-	app.put("/api/pirates/:id", PirateController.updatePirate);
-	app.delete("/api/pirates/:id", PirateController.deletePirate);
+	app.get("/api/pirate", PirateController.index);
+	app.post("/api/pirate/new", PirateController.createPirate);
+	app.get("/api/pirate/:id", PirateController.singlePirate);
+	app.put("/api/pirate/:id", PirateController.updatePirate);
+	app.delete("/api/pirate/:id", PirateController.deletePirate);
 };
