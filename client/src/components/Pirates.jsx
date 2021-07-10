d4c89199103b161827e03d06b991cdd60808df72
Kim Greenbush <kim_greenbush@icloud.com>
2021-07-10 13:27:39 -0700

    fix possible undefined error for "pirates"

--- client/src/components/Pirates.jsx
+++ client/src/components/Pirates.jsx
@@ -13,12 +13,12 @@ const Pirates = ({ pirates, setPirates}) => {
 
     return (
         <div>
-            {pirates.sort((a, b) => a.name.localeCompare(b.name)).map((pirate, idx) =>
+            {pirates? pirates.sort((a, b) => a.name.localeCompare(b.name)).map((pirate, idx) =>
                 <div key={idx}>
                     <img src={pirate.image} style={style} alt="pirate"/>
                     <p>{pirate.name}</p>
                     <p><Link to={`/pirate/${pirate._id}`}><button>View Pirate</button></Link> <DeleteButton pirateId={pirate._id} successCallback={() => removePirate(pirate._id)} /></p>
-                </div>)}
+                </div>) : ""}
         </div>
     )
 }
